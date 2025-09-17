6495bb507e2803b278695d5f9be02a74f30056ec
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2025-09-17 09:32:01 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ChakraProvider,  Box, extendTheme,} from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-
 import Homepage from './Components/Home/Homepage'
 import Dashboard from './Components/Dashboard/Dashboard';
 import Login from './Components/Login/Login';
@@ -11,27 +10,16 @@ import Contato from './Components/Contato/Contato';
 import Consulta from './Components/Consulta/ConsultaMain';
 import Downloads from './Components/Downloads/Downloads';
 import Dashboard_Math from './Components/Dashboard/Dashboard_Math';
-
 import Dashboard_Admin from './Components/Dashboard/Dashboard_Admin';
 import FAQSection from './Components/Home/FAQSection'
 
-
-
-
-
-
-
-
 const isAuthenticated = () => {
-  // Verifica se há um token de autenticação no localStorage
   return localStorage.getItem("token") !== null;
 };
 
 const ProtectedRoute = ({ element, ...rest }) => {
   return isAuthenticated() ? element : <Navigate to="/login" />;
 };
-
-
 
 const colors = {
   brand: {
@@ -41,7 +29,6 @@ const colors = {
   },
 };
 const theme = extendTheme({ colors });
-
 
 function App() {
   return (
@@ -54,36 +41,20 @@ function App() {
                 <Route path="/contato" element={<Contato />} />
                 <Route path="/consulta" element={<Consulta />} />
                 <Route path="/faq" element={<FAQSection />} />
-
-
-
-
                 <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />}/>
                 <Route path="/dashboard_math" element={<ProtectedRoute element={<Dashboard_Math />} />}/>
-
                 <Route path="/dashboard_admin" element={<ProtectedRoute element={<Dashboard_Admin />} />}/>
-
-
-
                 <Route path="*" element={<Navigate to="/login" />} />
-
                 <Route path="/chamado" element={<Chamado/>} />
                 <Route path="/downloads" element={<Downloads/>} />
 
                 <Route path="/finalizados" element={<FinalMain/>} />
-
-
-
-
-             </Routes>
-            
+             </Routes>         
           </Box>
       </ChakraProvider>
 
     </Router>
     
-
-   
   );
 }
 
