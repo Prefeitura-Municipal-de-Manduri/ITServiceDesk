6ff38050216fd38eac7f6f23f2369363e785aaa9
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2025-09-18 10:44:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Contato from './Components/Contato/Contato';
 import Consulta from './Components/Consulta/ConsultaMain';
 import Downloads from './Components/Downloads/Downloads';
 import Dashboard_Math from './Components/Dashboard/Dashboard_Math';
-import Dashboard_Admin from './Components/Dashboard/Dashboard_Admin';
 import FAQSection from './Components/Home/FAQSection'
 
 const isAuthenticated = () => {
@@ -43,7 +42,6 @@ function App() {
                 <Route path="/faq" element={<FAQSection />} />
                 <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />}/>
                 <Route path="/dashboard_math" element={<ProtectedRoute element={<Dashboard_Math />} />}/>
-                <Route path="/dashboard_admin" element={<ProtectedRoute element={<Dashboard_Admin />} />}/>
                 <Route path="*" element={<Navigate to="/login" />} />
                 <Route path="/chamado" element={<Chamado/>} />
                 <Route path="/downloads" element={<Downloads/>} />
